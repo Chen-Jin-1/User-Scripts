8a684906e37212a61a44312745e2d2e4d43730e1
Chen-Jin <cj3663@outlook.com>
2025-11-29 11:56:42 +0800

    Update sendSMS.user.js

--- sendSMS.user.js
+++ sendSMS.user.js
@@ -3,10 +3,10 @@
 // @namespace           cj-send-sms
 // @match               https://www.ccw.site/profile/*
 // @grant               none
-// @version             1.0.1
+// @version             1.0.2
 // @author              Chen-Jin
 // @description         使国外号码绑定的 CCW 账号能够发送验证码
-// @icon                https://m.ccw.site/user_projects_assets/c496b3fd-a476-4f1d-8f15-c77b17d30f11.png?x-oss-process=image/circle,r_150
+// @icon                https://m.ccw.site/user_projects_assets/c496b3fd-a476-4f1d-8f15-c77b17d30f11.png?x-oss-procss=image/circle,r_150
 // @updateURL           https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
 // @downloadURL         https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
 // @run-at              document-start
@@ -21,8 +21,8 @@ XHR.send = function (body) {
 		if (this.__sentry_xhr__.url.includes("sso.ccw.site/web/auth/assistant/captcha/v2/create")) {
 			let json = JSON.parse(body);
 			json.countryCode = countryCode;
-			return originalSend.call(this, JSON.stringify(json));
 			window.alert(`验证码已发送至 +${countryCode} ${json.recipient}`);
+			return originalSend.call(this, JSON.stringify(json));
 		}
 		return originalSend.apply(this, arguments);
 	} catch(e) {
