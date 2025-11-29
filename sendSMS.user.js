bdd5d698c899ec883cbe2b563a0313cf563f2631
Chen-Jin <cj3663@outlook.com>
2025-11-29 08:31:38 +0800

    Create sendSMS.user.js

---
+++ sendSMS.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name                发送验证码
+// @namespace           cj-send-sms
+// @match               https://www.ccw.site/profile/*
+// @grant               none
+// @version             1.0.0
+// @author              Chen-Jin
+// @description         使国外号码绑定的 CCW 账号能够发送验证码
+// @icon                https://m.ccw.site/user_projects_assets/c496b3fd-a476-4f1d-8f15-c77b17d30f11.png
+// @updateURL           https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
+// @downloadURL         https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
+// @run-at              document-start
+// ==/UserScript==
+
+var countryCode = "1" // 国家区号，可修改
+
+const XHR = XMLHttpRequest.prototype;
+const originalSend = XHR.send;
+XHR.send = function (body) {
+	try {
+		if (this.__sentry_xhr__.url.includes("sso.ccw.site/web/auth/assistant/captcha/v2/create")) {
+			let json = JSON.parse(body);
+			json.countryCode = countryCode;
+			return originalSend.call(this, JSON.stringify(json));
+		}
+		return originalSend.apply(this, arguments);
+	} catch(e) {
+		console.error(e, this);
+	}
+};
