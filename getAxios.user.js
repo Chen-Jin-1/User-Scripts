692d25e122c4a173e227a30d6a73599b443cc496
Chen-Jin <cj3663@outlook.com>
2025-11-30 14:24:24 +0800

    Create getAxios.user.js

---
+++ getAxios.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         获取axios
+// @version      1.0.0
+// @description  在ccw获取axios
+// @author       不想上学
+// @match        https://*.ccw.site/*
+// @icon         https://m.ccw.site/community/images/logo-ccw.png
+// @grant        unsafeWindow
+// @run-at       document-start
+// ==/UserScript==
+(function(){
+    const _apply = Function.prototype.apply;
+    Function.prototype.apply = function(thisArg, args) {
+        if (typeof thisArg === "object" && thisArg && thisArg.defaults && thisArg.interceptors && thisArg.interceptors.request.handlers.length > 0) {
+            console.log("axios", unsafeWindow.axios = thisArg);
+            Function.prototype.apply = _apply;
+        }
+        return _apply.call(this, thisArg, args);
+    };
+})()
