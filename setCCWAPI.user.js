f8bc8d8b811bbf0b71ba0144143d91fb6188ea83
Chen-Jin <cj3663@outlook.com>
2025-12-28 08:39:15 +0800

    Update setCCWAPI.user.js

--- setCCWAPI.user.js
+++ setCCWAPI.user.js
@@ -1,25 +1,24 @@
 // ==UserScript==
-// @name                挟持 CCW API
+// @name                修改 CCW API
 // @namespace           cj-set-ccw-api
 // @match               https://www.ccw.site/detail/*
 // @grant               none
-// @version             1.0.1
+// @version             1.0.2
 // @author              Chen-Jin
-// @description         防止作品使用连接社区扩展骗币等
-// @icon                https://m.ccw.site/user_projects_assets/6df19377-8e84-4929-9e9c-93ddca968dcc.png
-// @updateURL           https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/setCCWAPI.user.js
-// @downloadURL         https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/setCCWAPI.user.js
+// @description         只对连接社区扩展的一些功能有效
+// @icon                data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTI5IiBoZWlnaHQ9IjEyOSIgdmlld0JveD0iMCAwIDEyOSAxMjkiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IHg9IjM3IiB5PSI0MCIgd2lkdGg9IjUyIiBoZWlnaHQ9IjUyIiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjYiLz4KPHJlY3QgeD0iMjQuNSIgeT0iMjcuNSIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0IiByeD0iMy41IiBmaWxsPSIjRkFGRjAwIiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjMiLz4KPHJlY3QgeD0iMjQuNSIgeT0iNzkuNSIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0IiByeD0iMy41IiBmaWxsPSIjNzFFQTM5IiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjMiLz4KPHJlY3QgeD0iNzYuNSIgeT0iMjcuNSIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0IiByeD0iMy41IiBmaWxsPSIjRkY0OTRCIiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjMiLz4KPHJlY3QgeD0iNzYuNSIgeT0iNzkuNSIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0IiByeD0iMy41IiBmaWxsPSIjNzVENkZEIiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjMiLz4KPC9zdmc+Cg==
+// @updateURL           https://gh.llkk.cc/https://github.com/Chen-Jin-1/User-Scripts/raw/refs/heads/main/setCCWAPI.user.js
+// @downloadURL         https://gh.llkk.cc/https://github.com/Chen-Jin-1/User-Scripts/raw/refs/heads/main/setCCWAPI.user.js
 // @run-at              document-start
 // ==/UserScript==
-
+​
 var config = {
-    // 可修改此对象
-    coins: 1000, // 投币数
+    coins: 1000, // 修改后的投币数
 };
-
+​
 function getCCWAPI() {
     return new Promise(resolve => {
-        var x = setInterval(() => {
+        const x = setInterval(() => {
             if (window.Scratch?.runtime?.ccwAPI?.isFollowed) {
                 clearInterval(x);
                 resolve(Scratch.runtime.ccwAPI);
@@ -27,45 +26,45 @@ function getCCWAPI() {
         })
     })
 }
-
+​
 (async function() {
-    var ccwAPI = await getCCWAPI();
-    console.log("挟持 CCW API 脚本已获取到 CCW API 并挟持");
+    const ccwAPI = await getCCWAPI();
+    console.log("setCCWAPI 已获取到 CCWAPI");
     setInterval(() => {
         ccwAPI.isFollowed = async (arg) => {
-            console.log(`已挟持 是 ${arg} 粉丝`);
+            console.log(`是 ${arg} 粉丝`);
             return true;
         };
         ccwAPI.isMyFans = async () => {
-            console.log(`已挟持 是作者粉丝`);
+            console.log(`是作者粉丝`);
             return true;
         };
         ccwAPI.isLiked = async () => {
-            console.log(`已挟持 已点赞`);
+            console.log(`已点赞`);
             return true;
         };
         ccwAPI.redirect = async (arg) => {
-            console.log(`已挟持 跳转到 https://ccw.site/${arg}`);
+            console.log(`跳转到 https://ccw.site/${arg}`);
             return true;
         };
         ccwAPI.requestCoins = async (arg) => {
-            console.log(`已挟持 请求投 ${arg} 币`);
+            console.log(`请求投 ${arg} 币`);
             return true;
         };
         ccwAPI.requestFollow = async () => {
-            console.log(`已挟持 请求关注作者`);
+            console.log(`请求关注作者`);
             return true;
         };
         ccwAPI.getCoinCount = async () => {
-            console.log(`已挟持 获取投币数 为 ${config.coins}`);
+            console.log(`获取投币数 为 ${config.coins}`);
             return config.coins;
         };
         ccwAPI.isLikedProject = async (arg) => {
-            console.log(`已挟持 点赞作品 ${arg}`);
+            console.log(`作品 ${arg} 是否点赞`);
             return true;
         };
         ccwAPI.isFavoriteProject = async (arg) => {
-            console.log(`已挟持 收藏作品 ${arg}`);
+            console.log(`作品 ${arg} 是否收藏`);
             return true;
         };
     }, 1000);
