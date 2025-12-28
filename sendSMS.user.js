66f1fb7ffbbfac67c065aaeadb750c4572531c94
Chen-Jin <cj3663@outlook.com>
2025-12-28 12:46:32 +0800

    Update sendSMS.user.js

--- sendSMS.user.js
+++ sendSMS.user.js
@@ -3,16 +3,15 @@
 // @namespace           cj-send-sms
 // @match               https://www.ccw.site/profile/*
 // @grant               none
-// @version             1.0.2
+// @version             1.0.3
 // @author              Chen-Jin
 // @description         使国外号码绑定的 CCW 账号能够发送验证码
-// @icon                https://m.ccw.site/user_projects_assets/c496b3fd-a476-4f1d-8f15-c77b17d30f11.png?x-oss-procss=image/circle,r_150
-// @updateURL           https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
-// @downloadURL         https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
+// @icon                https://m.ccw.site/community/images/logo-ccw.png
+// @updateURL           https://gh.llkk.cc/https://github.com/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
+// @downloadURL         https://gh.llkk.cc/https://github.com/Chen-Jin-1/User-Scripts/raw/refs/heads/main/sendSMS.user.js
 // @run-at              document-start
 // ==/UserScript==
-
-var countryCode = "1" // 国家区号，可修改
+if (!countryCode) window.countryCode = "1";
 
 const XHR = XMLHttpRequest.prototype;
 const originalSend = XHR.send;
@@ -21,7 +20,7 @@ XHR.send = function (body) {
 		if (this.__sentry_xhr__.url.includes("sso.ccw.site/web/auth/assistant/captcha/v2/create")) {
 			let json = JSON.parse(body);
 			json.countryCode = countryCode;
-			window.alert(`验证码已发送至 +${countryCode} ${json.recipient}`);
+			if (json.captcha) console.log(`发送验证码至 +${countryCode} ${json.recipient}`);
 			return originalSend.call(this, JSON.stringify(json));
 		}
 		return originalSend.apply(this, arguments);
