10aedf1e7392c3c1d0b7e9b672b81e37622a9a1b
Chen-Jin <cj3663@outlook.com>
2026-01-02 17:59:12 +0800

    Create cmDl.user.js

---
+++ cmDl.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         从外链式生成器下载音乐
+// @namespace    cj-cm-op-dl
+// @version      1.0.0
+// @description  打开外链式生成器即可下载
+// @match        https://music.163.com/outchain/player*
+// @grant        GM_download
+// @run-at       document-start
+// @icon         https://music.163.com/favicon.ico
+// @author       Chen-Jin
+// ==/UserScript==
+
+const srcSetter = Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'src').set;
+Object.defineProperty(HTMLMediaElement.prototype, 'src', {
+    set(value) {
+        if (value) {
+            setTimeout(() => {
+                GM_download({
+                  name: document.getElementById("title").innerText + '.mp3',
+                  url: this.src,
+                });
+            }, 1000)
+        }
+        return srcSetter.call(this, value);
+    },
+    get: Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'src').get,
+    configurable: true
+});
