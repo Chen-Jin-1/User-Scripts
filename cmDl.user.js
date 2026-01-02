7ebe36c15668337a461aa1b1cf4bafc2558f3e97
Chen-Jin <cj3663@outlook.com>
2026-01-02 18:35:31 +0800

    Update cmDl.user.js

--- cmDl.user.js
+++ cmDl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         网易云音乐下载
 // @namespace    cj-cm-dl
-// @version      1.0.2
+// @version      1.0.3
 // @description  打开外链式生成器即可下载
 // @match        https://music.163.com/outchain/player*
 // @grant        GM_download
@@ -11,14 +11,14 @@
 // @updateURL    https://gh.llkk.cc/https://github.com/Chen-Jin-1/User-Scripts/raw/refs/heads/main/cmDl.user.js
 // @downloadURL  https://gh.llkk.cc/https://github.com/Chen-Jin-1/User-Scripts/raw/refs/heads/main/cmDl.user.js
 // ==/UserScript==
-​
+
 const srcSetter = Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'src').set;
 Object.defineProperty(HTMLMediaElement.prototype, 'src', {
     set(value) {
         if (value) {
             setTimeout(() => {
                 GM_download({
-                  name: document.getElementById("title").innerText + '.mp3',
+                  name: document.getElementById("title").innerText.replaceAll("/", "、") + '.mp3',
                   url: this.src,
                 });
             }, 100);
