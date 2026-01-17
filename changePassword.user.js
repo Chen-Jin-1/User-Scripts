b46e6a8e45889f8a3cc3a097d82baedc5148f9d0
Chen-Jin <cj3663@outlook.com>
2026-01-18 01:27:51 +0800

    Update changePassword.user.js

--- changePassword.user.js
+++ changePassword.user.js
@@ -7,8 +7,8 @@
 // @author              Chen-Jin
 // @description         使国外号码绑定的 CCW 账号能够发送验证码
 // @icon                https://m.ccw.site/community/images/logo-ccw.png
-// @updateURL           https://us.chen-jin.dpdns.org/changePassword.js
-// @downloadURL         https://us.chen-jin.dpdns.org/changePassword.js
+// @updateURL           https://us.chen-jin.dpdns.org/changePassword.user.js
+// @downloadURL         https://us.chen-jin.dpdns.org/changePassword.user.js
 // @run-at              document-start
 // ==/UserScript==
 if (!window.countryCode) window.countryCode = "1";
