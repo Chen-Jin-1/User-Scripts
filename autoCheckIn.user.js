f27b30a884578e912c67a489b95cee997da48911
Chen-Jin <cj3663@outlook.com>
2026-02-03 20:30:03 +0800

    不登录不签到

--- autoCheckIn.user.js
+++ autoCheckIn.user.js
@@ -12,6 +12,8 @@
 // @run-at            document-start
 // ==/UserScript==
 
+if (!document.cookie.includes("cookie-user-id")) throw "";
+
 const _apply = Function.prototype.apply;
 Function.prototype.apply = function(thisArg, args) {
     if (typeof thisArg === "object" && thisArg && thisArg.defaults && thisArg.interceptors && thisArg.interceptors.request.handlers.length > 0) {
@@ -34,48 +36,46 @@ Function.prototype.apply = function(thisArg, args) {
     return _apply.call(this, thisArg, args);
 };
 
-if (document.cookie.includes("cookie-user-id")) {
-    const originSend = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function (body) {
-        if (this.__sentry_xhr__?.url === "https://community-web.ccw.site/task/mine") {
-            // const originalOnReadyStateChange = this.onreadystatechange;
-            // this.onreadystatechange = function() {
-            //     const response = JSON.parse(this.response);
-            //     response.body = response.body.map(task => {
-            //         if (task.status === "PREPARED") task.status = "CYCLE";
-            //         return task;
-            //     });
+const originSend = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.send = function (body) {
+    if (this.__sentry_xhr__?.url === "https://community-web.ccw.site/task/mine") {
+        // const originalOnReadyStateChange = this.onreadystatechange;
+        // this.onreadystatechange = function() {
+        //     const response = JSON.parse(this.response);
+        //     response.body = response.body.map(task => {
+        //         if (task.status === "PREPARED") task.status = "CYCLE";
+        //         return task;
+        //     });
 
-            //     const modifiedResponse = JSON.stringify(response);
-            //     Object.defineProperties(this, {
-            //         'responseText': {
-            //             value: modifiedResponse,
-            //             writable: false,
-            //             configurable: true
-            //         },
-            //         'response': {
-            //             value: modifiedResponse,
-            //             writable: false,
-            //             configurable: true
-            //         }
-            //     });
-            //     return originalOnReadyStateChange.apply(this, arguments);
-            // }
-            this.addEventListener("load", () => {
-                XMLHttpRequest.prototype.send = originSend;
-                let tasks = JSON.parse(this.response).body.filter(task => task.status === "PREPARED");
-                tasks.forEach(task => {
-                    fetch("https://community-web.ccw.site//task/award", {
-                        body: JSON.stringify({taskOid: task.oid}),
-                        method: 'post',
-                        credentials: 'include',
-                        headers: {'content-type': 'application/json'}
-                    })
-                        .then(response => response.json())
-                        .then(({body}) => console.log(`已领取任务 ${task.name}，获得 ${body.rewareValue} 金币`));
-                });
-            })
-        }
-        return originSend.call(this, body);
+        //     const modifiedResponse = JSON.stringify(response);
+        //     Object.defineProperties(this, {
+        //         'responseText': {
+        //             value: modifiedResponse,
+        //             writable: false,
+        //             configurable: true
+        //         },
+        //         'response': {
+        //             value: modifiedResponse,
+        //             writable: false,
+        //             configurable: true
+        //         }
+        //     });
+        //     return originalOnReadyStateChange.apply(this, arguments);
+        // }
+        this.addEventListener("load", () => {
+            XMLHttpRequest.prototype.send = originSend;
+            let tasks = JSON.parse(this.response).body.filter(task => task.status === "PREPARED");
+            tasks.forEach(task => {
+                fetch("https://community-web.ccw.site//task/award", {
+                    body: JSON.stringify({taskOid: task.oid}),
+                    method: 'post',
+                    credentials: 'include',
+                    headers: {'content-type': 'application/json'}
+                })
+                    .then(response => response.json())
+                    .then(({body}) => console.log(`已领取任务 ${task.name}，获得 ${body.rewareValue} 金币`));
+            });
+        })
     }
+    return originSend.call(this, body);
 }
