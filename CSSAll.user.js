d4f0aaf3a39de0c8aed4d3315dc45b2cc9044078
Chen-Jin <cj3663@outlook.com>
2026-06-21 04:44:49 +0800

    更新 index.html、新增 CSSAll

---
+++ CSSAll.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name                CSS 合集
+// @namespace           cj-hide-warn
+// @match               https://www.ccw.site/*
+// @match               https://turbowarp.org/*
+// @grant               none
+// @version             1.0.0
+// @author              Chen-Jin
+// @description         CSS 注入脚本集合
+// @icon                https://m.ccw.site/community/images/logo-ccw.png
+// @downloadURL         https://us.chen-jin.dpdns.org/hideWarn.user.js
+// @run-at              document-start
+// ==/UserScript==
+const x = document.createElement("style")
+x.innerHTML = `.ReactModal__Overlay.ReactModal__Overlay--after-open.gandi_webgl-modal_modal-overlay_2yidY, .ReactModal__Overlay.ReactModal__Overlay--after-open.browser-modal_modal-overlay_3TDyF {display: none}
+div[class*="action-control-"] {visibility: visible !important;}
+.leftContent-3zu6j>p, .bioDisplay-3PigT, .signature-1K1p- {max-height: 10vh; overflow-y: scroll}`;
+document.head.appendChild(x);
