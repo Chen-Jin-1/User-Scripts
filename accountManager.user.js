e4670d2d049c452d6c525ddeb8e1365f86256db9
Chen-Jin <cj3663@outlook.com>
2026-07-10 20:49:28 +0800

    修复账号管理器语法错误

--- accountManager.user.js
+++ accountManager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              账号管理器
 // @namespace         cj-auto-check-in
-// @version           1.2.6
+// @version           1.2.7
 // @description       快捷切换 CCW 账号
 // @author            Chen-Jin
 // @match             https://*.ccw.site/*
@@ -93,7 +93,7 @@ if (document.cookie.includes("cookie-user-id")) {
     unsafeWindow.cjam_c = 1;
     XMLHttpRequest.prototype.open = function(m, u, a) {
         if (u === "https://community-web.ccw.site/students/self/detail") this.addEventListener("load", () => {
-            unsafeWindow.cjam_c && XMLHttpRequest.prototype.open = _open;
+            unsafeWindow.cjam_c && (XMLHttpRequest.prototype.open = _open);
             let json = JSON.parse(this.response).body;
             if (json) {
                 currentId = json.studentNumber;
